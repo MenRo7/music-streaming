d9b25b651ac81eb44a103abedb8bb4f99bc91001
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2025-01-16 15:12:12 +0100

    wip auth page

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,43 @@ import SongPlayer from './components/SongPlayer';
 import PlaylistPage from './pages/PlaylistPage';
 import ProfilePage from './pages/ProfilePage';
 import MainPage from './pages/MainPage';
+import AuthPage from './pages/AuthPage';  // Importer AuthPage
 
 import './App.css';
 
 const App: React.FC = () => (
   <Router>
     <div className="app">
-      <Navbar />
-      <Sidebar />
       <Routes>
-        <Route path="/playlist/:id" element={<PlaylistPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/" element={<MainPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        
+        <Route path="/playlist/:id" element={
+          <>
+            <Navbar />
+            <Sidebar />
+            <PlaylistPage />
+            <SongPlayer audioUrl="" />
+          </>
+        } />
+
+        <Route path="/profile" element={
+          <>
+            <Navbar />
+            <Sidebar />
+            <ProfilePage />
+            <SongPlayer audioUrl="" />
+          </>
+        } />
+
+        <Route path="/" element={
+          <>
+            <Navbar />
+            <Sidebar />
+            <MainPage />
+            <SongPlayer audioUrl="" />
+          </>
+        } />
       </Routes>
-      <SongPlayer audioUrl="" />
     </div>
   </Router>
 );
